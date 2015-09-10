8110f58d9dfc21294a3364037d372788e889a463
Rishi Rajasekaran <rajasekaran.rishi@gmail.com>
2015-09-10 15:56:11 +0530

    Update Image class for storing images in file

--- app/Image.php
+++ app/Image.php
@@ -2,10 +2,25 @@
 
 namespace App;
 
+use Storage;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
     //
     protected $table = 'images';
+
+    public static function saveImage($image_encoded) {
+      $image_stripped = str_replace('data:image/png;base64,', '', $image_encoded);
+    	$image_stripped = str_replace(' ', '+', $image_stripped);
+    	$image_data = base64_decode($image_stripped);
+      $image_url = uniqid() . '.png';
+      try {
+        Storage::disk('local')->put($image_url, $image_data);
+        return $image_url;
+      } catch (Exception $e) {
+        return false;
+      }
+    }
 }
