57f0860e4f6be85f05bec7af67af175aec9d843c
Siddarth Iyer <mindstormer619@gmail.com>
2016-09-12 18:34:45 +0530

    Add comments; convert photobooth.js to tabs

--- public/js/photobooth.js
+++ public/js/photobooth.js
@@ -1,276 +1,276 @@
 var camera = (function(args) {
-  var width = args.width || 640;
-  var height = args.height || 480;
+	var width = args.width || 640;
+	var height = args.height || 480;
 
-  var shutterCallback = args.shutterCallback || function() {};
+	var shutterCallback = args.shutterCallback || function() {};
 
-  var streamObj;
+	var streamObj;
 
-  var canvas = document.getElementById(args.canvasId);
-  var hiddenCanvas = document.getElementById(args.hiddenCanvasId);
-  var video = document.getElementById(args.videoId);
-  var shutter = document.getElementById(args.shutterId);
-  var shutterTimer = document.getElementById(args.shutterTimer);
-  var cameraStopper = document.getElementById(args.cameraStopperId);
+	var canvas = document.getElementById(args.canvasId);
+	var hiddenCanvas = document.getElementById(args.hiddenCanvasId);
+	var video = document.getElementById(args.videoId);
+	var shutter = document.getElementById(args.shutterId);
+	var shutterTimer = document.getElementById(args.shutterTimer);
+	var cameraStopper = document.getElementById(args.cameraStopperId);
 
-  var context = canvas.getContext('2d');
-  var hiddenContext = hiddenCanvas.getContext('2d');
-  var videoObject = { video: true };
+	var context = canvas.getContext('2d');
+	var hiddenContext = hiddenCanvas.getContext('2d');
+	var videoObject = { video: true };
 
-  var errorCallback = function(error) {
-    console.log('Video capture error: ', error.code);
-  };
+	var errorCallback = function(error) {
+		console.log('Video capture error: ', error.code);
+	};
 
-  var images = [];
-  var initializeWebcam = function() {
-    // Initialized as a singleton
-    if (navigator.getUserMedia) {
-      navigator.getUserMedia(videoObject, function(stream) {
-        streamObj = stream;
-        video.src = stream;
-        video.play();
-      }, errorCallback);
-    } else if (navigator.webkitGetUserMedia) {
-      navigator.webkitGetUserMedia(videoObject, function(stream) {
-        streamObj = stream;
-        video.src = window.URL.createObjectURL(stream);
-        video.play();
-      }, errorCallback);
-    } else if (navigator.mozGetUserMedia) {
-      navigator.mozGetUserMedia(videoObject, function(stream) {
-        streamObj = stream;
-        video.src = window.URL.createObjectURL(stream);
-        video.play();
-      }, errorCallback);
-    }
-  };
+	var images = [];
+	var initializeWebcam = function() {
+		// Initialized as a singleton
+		if (navigator.getUserMedia) {
+			navigator.getUserMedia(videoObject, function(stream) {
+				streamObj = stream;
+				video.src = stream;
+				video.play();
+			}, errorCallback);
+		} else if (navigator.webkitGetUserMedia) {
+			navigator.webkitGetUserMedia(videoObject, function(stream) {
+				streamObj = stream;
+				video.src = window.URL.createObjectURL(stream);
+				video.play();
+			}, errorCallback);
+		} else if (navigator.mozGetUserMedia) {
+			navigator.mozGetUserMedia(videoObject, function(stream) {
+				streamObj = stream;
+				video.src = window.URL.createObjectURL(stream);
+				video.play();
+			}, errorCallback);
+		}
+	};
 
-  // Resets the canvas
-  var clearCanvas = function() {
-    context.clearRect(0, 0, width, height);
-    hiddenContext.clearRect(0, 0, width, height);
-  };
+	// Resets the canvas
+	var clearCanvas = function() {
+		context.clearRect(0, 0, width, height);
+		hiddenContext.clearRect(0, 0, width, height);
+	};
 
-  // Clicking this button emulates the shutter button
-  shutter.addEventListener('click', function() {
+	// Clicking this button emulates the shutter button
+	shutter.addEventListener('click', function() {
 
-    var TIME_DELAY = 7;
-    var INTERVAL = 1000;
-    var MILLISECOND_DELAY = TIME_DELAY * INTERVAL;
-    var countdownInterval = setInterval(function() {
-      if (TIME_DELAY == 0) {
-        hiddenContext.drawImage(video, 0, 0, width, height);
-        context.drawImage(video, 0, 0, width, height);
-        shutterCallback();
-        clearInterval(countdownInterval);
-        shutterTimer.innerHTML = '';
-        return;
-      }
-      shutterTimer.innerHTML = "00:0"+TIME_DELAY;
-      TIME_DELAY--;
-    }, INTERVAL);
-  });
+		var TIME_DELAY = 7;
+		var INTERVAL = 1000;
+		var MILLISECOND_DELAY = TIME_DELAY * INTERVAL;
+		var countdownInterval = setInterval(function() {
+			if (TIME_DELAY == 0) {
+				hiddenContext.drawImage(video, 0, 0, width, height);
+				context.drawImage(video, 0, 0, width, height);
+				shutterCallback();
+				clearInterval(countdownInterval);
+				shutterTimer.innerHTML = '';
+				return;
+			}
+			shutterTimer.innerHTML = "00:0"+TIME_DELAY;
+			TIME_DELAY--;
+		}, INTERVAL);
+	});
 
-  cameraStopper.addEventListener('click', function() {
-    // Clear video source
-    streamObj.getTracks()[0].stop();
-    video.src = '';
-    args.cameraStopperCallback();
-  });
-  // Creates an image object out of the contents of the canvas
-  var getImageFromHiddenCanvas = function() {
-    var image = new Image();
-    image.src = hiddenCanvas.toDataURL('image/png');
-    return image;
-  };
+	cameraStopper.addEventListener('click', function() {
+		// Clear video source
+		streamObj.getTracks()[0].stop();
+		video.src = '';
+		args.cameraStopperCallback();
+	});
+	// Creates an image object out of the contents of the canvas
+	var getImageFromHiddenCanvas = function() {
+		var image = new Image();
+		image.src = hiddenCanvas.toDataURL('image/png');
+		return image;
+	};
 
-  var getImageFromCanvas = function() {
-    var image = new Image();
-    image.src = canvas.toDataURL('image/png');
-    return image;
-  };
+	var getImageFromCanvas = function() {
+		var image = new Image();
+		image.src = canvas.toDataURL('image/png');
+		return image;
+	};
 
-  var saveImage = function() {
-    var image = getImageFromCanvas();
-    clearCanvas();
-    args.saveImageCallback();
-    uploadImage(image);
-  };
+	var saveImage = function() {
+		var image = getImageFromCanvas();
+		clearCanvas();
+		args.saveImageCallback();
+		uploadImage(image);
+	};
 
-  var rejectImage = function() {
-    camera.clearCanvas();
-    args.rejectImageCallback();
-  };
+	var rejectImage = function() {
+		camera.clearCanvas();
+		args.rejectImageCallback();
+	};
 
-  var applyFilter = function(filterName) {
-    context.drawImage(hiddenCanvas, 0, 0, width, height);
-    Caman(
-      canvas,
-      function() {
-        this.reloadCanvasData();
-        switch (filterName) {
-          case 'vintage':
-            this.vintage();
-            break;
-          case 'lomo':
-            this.lomo();
-            break;
-          case 'sunrise':
-            this.sunrise();
-            break;
-          case 'grungy':
-            this.grungy();
-            break;
-          case 'oldBoot':
-            this.oldBoot();
-            break;
-          case 'glowingSun':
-            this.glowingSun();
-            break;
-          case 'concentrate':
-            this.concentrate();
-            break;
-        }
-        this.render();
-      }
-    );
-  };
+	var applyFilter = function(filterName) {
+		context.drawImage(hiddenCanvas, 0, 0, width, height);
+		Caman(
+			canvas,
+			function() {
+				this.reloadCanvasData();
+				switch (filterName) {
+					case 'vintage':
+						this.vintage();
+						break;
+					case 'lomo':
+						this.lomo();
+						break;
+					case 'sunrise':
+						this.sunrise();
+						break;
+					case 'grungy':
+						this.grungy();
+						break;
+					case 'oldBoot':
+						this.oldBoot();
+						break;
+					case 'glowingSun':
+						this.glowingSun();
+						break;
+					case 'concentrate':
+						this.concentrate();
+						break;
+				}
+				this.render();
+			}
+		);
+	};
 
-  return {
-    video: video,
-    initializeWebcam: initializeWebcam,
-    getImageFromCanvas: getImageFromCanvas,
-    clearCanvas: clearCanvas,
-    applyFilter: applyFilter,
-    getImageFromHiddenCanvas: getImageFromHiddenCanvas,
-    saveImage: saveImage,
-    rejectImage: rejectImage,
-    images: images,
-  };
+	return {
+		video: video,
+		initializeWebcam: initializeWebcam,
+		getImageFromCanvas: getImageFromCanvas,
+		clearCanvas: clearCanvas,
+		applyFilter: applyFilter,
+		getImageFromHiddenCanvas: getImageFromHiddenCanvas,
+		saveImage: saveImage,
+		rejectImage: rejectImage,
+		images: images,
+	};
 }
 )({
-  canvasId: 'canvas',
-  hiddenCanvasId: 'temp-canvas',
-  videoId: 'camera',
-  shutterId: 'snap',
-  shutterTimer: 'timer-text',
-  shutterCallback: function() {
-    document.getElementById('camera-frame').style.display = 'none';
-    document.getElementById('snap-choice').style.display = 'block';
-    document.getElementById('photo-frame').style.display = 'block';
-  },
-  saveImageCallback: function() {
-    document.getElementById('camera-frame').style.display = 'block';
-    document.getElementById('photo-frame').style.display = 'none';
-  },
-  rejectImageCallback: function() {
-    document.getElementById('camera-frame').style.display = 'block';
-    document.getElementById('photo-frame').style.display = 'none';
-  },
-  cameraStopperId: 'end',
-  cameraStopperCallback: function() {
-    document.getElementById('response-box').style.display = 'none';
-    document.getElementById('snap-choice').style.display = 'none';
-    document.getElementById('photo-frame').style.display = 'none';
-    document.getElementById('card-input').value = '';
-    document.getElementById('card-input-box').style.display = 'block';
-    document.getElementById('card-input').focus();
-  }
+	canvasId: 'canvas',
+	hiddenCanvasId: 'temp-canvas',
+	videoId: 'camera',
+	shutterId: 'snap',
+	shutterTimer: 'timer-text',
+	shutterCallback: function() {
+		document.getElementById('camera-frame').style.display = 'none';
+		document.getElementById('snap-choice').style.display = 'block';
+		document.getElementById('photo-frame').style.display = 'block';
+	},
+	saveImageCallback: function() {
+		document.getElementById('camera-frame').style.display = 'block';
+		document.getElementById('photo-frame').style.display = 'none';
+	},
+	rejectImageCallback: function() {
+		document.getElementById('camera-frame').style.display = 'block';
+		document.getElementById('photo-frame').style.display = 'none';
+	},
+	cameraStopperId: 'end',
+	cameraStopperCallback: function() {
+		document.getElementById('response-box').style.display = 'none';
+		document.getElementById('snap-choice').style.display = 'none';
+		document.getElementById('photo-frame').style.display = 'none';
+		document.getElementById('card-input').value = '';
+		document.getElementById('card-input-box').style.display = 'block';
+		document.getElementById('card-input').focus();
+	}
 });
 
 function loadDetails(name, rollNo, festemberId) {
-  document.getElementById('student-name').innerHTML = name;
-  document.getElementById('roll-number').innerHTML = rollNo;
-  document.getElementById('festember-id').innerHTML = festemberId;
-  document.getElementById('response-box').style.display = 'block';
+	document.getElementById('student-name').innerHTML = name;
+	document.getElementById('roll-number').innerHTML = rollNo;
+	document.getElementById('festember-id').innerHTML = festemberId;
+	document.getElementById('response-box').style.display = 'block';
 }
 
 function submit(card) {
-  console.log('Submitted!');
-  $.ajax({
-    method: 'GET',
-    url: '/students/' + card,
-  })
-  .success(
-    function(response) {
-      console.log(response);
-      if (JSON.stringify(response) != '{}') {
-        loadDetails(response.name, response.roll_no, response.festember_id);
-        camera.initializeWebcam();
-        document.getElementById('card-input-box').style.display = 'none';
-      }
-    }
-  )
-  .error(
-      function() {
-        console.log('Request failed!');
-      }
-    );
+	console.log('Submitted!');
+	$.ajax({
+		method: 'GET',
+		url: '/students/' + card,
+	})
+	.success(
+		function(response) {
+			console.log(response);
+			if (JSON.stringify(response) != '{}') {
+				loadDetails(response.name, response.roll_no, response.festember_id);
+				camera.initializeWebcam();
+				document.getElementById('card-input-box').style.display = 'none';
+			}
+		}
+	)
+	.error(
+			function() {
+				console.log('Request failed!');
+			}
+		);
 }
 
 //Event handling for onkeydown
 function checkSubmission(event) {
-  var key = event.which || event.keyCode;
-  if (key == 13) {
-    var card = document.getElementById('card-input').value;
-    console.log(card);
-    if (typeof card !== 'undefined' && card !== '' && card !== null) {
-      submit(card);
-    }
-  }
+	var key = event.which || event.keyCode;
+	if (key == 13) {
+		var card = document.getElementById('card-input').value;
+		console.log(card);
+		if (typeof card !== 'undefined' && card !== '' && card !== null) {
+			submit(card);
+		}
+	}
 }
 
 function uploadImage(image) {
-  var festemberId = document.getElementById('festember-id').innerHTML;
-  var formData = new FormData();
-  formData.append('base64image', image.src);
-  formData.append('festember_id', festemberId);
+	var festemberId = document.getElementById('festember-id').innerHTML;
+	var formData = new FormData();
+	formData.append('base64image', image.src);
+	formData.append('festember_id', festemberId);
 
-  return $.ajax({
-    url: '/images/',
-    method: 'POST',
-    data: formData,
-    processData: false,
-    contentType: false,
-  });
+	return $.ajax({
+		url: '/images/',
+		method: 'POST',
+		data: formData,
+		processData: false,
+		contentType: false,
+	});
 }
 
 function initializeFilterButtons() {
-  // Names of buttons
-  var names = [
-    'None',
-    'Vintage',
-    'Lomo',
-    'Sunrise',
-    'Grungy',
-    'Old Boot',
-    'Glowing Sun',
-    'Concentrate',
-  ];
+	// Names of buttons
+	var names = [
+		'None',
+		'Vintage',
+		'Lomo',
+		'Sunrise',
+		'Grungy',
+		'Old Boot',
+		'Glowing Sun',
+		'Concentrate',
+	];
 
-  var filters = [
-    'none',
-    'vintage',
-    'lomo',
-    'sunrise',
-    'grungy',
-    'oldBoot',
-    'glowingSun',
-    'concentrate',
-  ];
+	var filters = [
+		'none',
+		'vintage',
+		'lomo',
+		'sunrise',
+		'grungy',
+		'oldBoot',
+		'glowingSun',
+		'concentrate',
+	];
 
-  for (var i = 0; i < names.length; i++) {
-    var buttonString =
-      '<button id="filter-button" ' +
-        'onclick="camera.applyFilter(\'' + filters[i] + '\')">' +
-        names[i] +
-      '</button> ';
-    $('#filter-choices').append(buttonString);
-  }
+	for (var i = 0; i < names.length; i++) {
+		var buttonString =
+			'<button id="filter-button" ' +
+				'onclick="camera.applyFilter(\'' + filters[i] + '\')">' +
+				names[i] +
+			'</button> ';
+		$('#filter-choices').append(buttonString);
+	}
 }
 
 document.getElementById('card-input').addEventListener('keypress', checkSubmission);
 
 document.addEventListener('DOMContentLoaded', function() {
-  initializeFilterButtons();
+	initializeFilterButtons();
 }, false);
--- resources/views/home.php
+++ resources/views/home.php
@@ -2,17 +2,25 @@
 <html>
 	<head>
 		<title>Festember Photobooth</title>
+
+		<!-- Styling -->
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/custom.css">
+		<!-- Fonts -->
 		<link href='https://fonts.googleapis.com/css?family=Archivo+Narrow:700,400' rel='stylesheet' type='text/css'>
 		<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 	</head>
+
 	<body style="overflow:hidden;">
+
 		<div class="row">
+			<!-- Heading -->
 			<img id="logo" src="logo.png">
 			<h1 align="center" class="heading">Festember Photobooth</h1>
 		</div>
+
 		<div id='card-input-box' class='row'>
+			<!-- Card number input -->
 			<div align='center'>
 				<label for='card'>Please scan the card</label>
 				<input type='password' id='card-input' name='card' autofocus>
@@ -20,7 +28,9 @@
 		</div>
 
 		<div class="row">
+			<!-- Photo snapping page: hidden initially -->
 			<div class='col-md-3 pull-left'>
+				<!-- Photo snap controls and timer -->
 				<div id="response-box" hidden>
 					<p>Hello,<br> <strong><span id='student-name' style="font-size:18px"></span></strong>!</p><br>
 					<p>Just to confirm, your Roll Number is<br> <strong><span id='roll-number' style="font-size:18px"></span></strong></p><br>
@@ -36,11 +46,13 @@
 			</div>
 
 			<div class="col-md-6" id="camera-frame">
+				<!-- Camera frame -->
 				<video id='camera' width='640' height='480' autoplay></video>
 			</div>
 
 			<div class="col-md-3 pull-right">
 				<div class='row' id='photo-frame' hidden >
+					<!-- Photo preview -->
 					<canvas id='temp-canvas' width='640' height='480' hidden></canvas>
 					<div id='filter-choices'></div>
 					<canvas id='canvas' width='640' height='480'></canvas>
@@ -52,127 +64,121 @@
 				</div>
 			</div>
 		</div>
+
+		<!-- Footer text -->
 		<p id="tag" align="center">Made with <span class="glyphicon glyphicon-heart"></span> by <span style="font-weight:700;font-size:20px;">Spider</span></p>
+
 		<!---For Background-->
 		<div style="position: fixed; left: 0; right: 0; top: 0; bottom: 0; z-index:-1;"></div>
 		<div>
 			<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 		</div>
+
+		<!-- JS Libraries -->
 		<script type='text/javascript' src='js/jquery-1.11.3.js'></script>
 		<script type='text/javascript' src='js/bootstrap.min.js'></script>
 		<script type='text/javascript' src='js/caman.full.js'></script>
+		<script src="js/geometryangle.min.js"></script>
+
+		<!-- Photobooth scripts -->
 		<script type='text/javascript' src='js/photobooth.js'></script>
-		<script src="js/geometryangle.js"></script>
-<script>
-$(document).ready(function(){
-	$('body').Geometryangle({
 
-		// handle transparent colors
-		mesh:{
+		<script>
+		$(document).ready(function(){
+			$('body').Geometryangle({
+				// handle transparent colors
+				mesh:{
+					width: 1.2,
+					height: 1.2,
 
-		  width: 1.2,
-		  height: 1.2,
+					// How far should the mesh vary into z-space.
+					depth: 10,
 
-		  // How far should the mesh vary into z-space.
-		  depth: 10,
+					// Number of columns for the mesh.
+					columns: undefined,
 
-		  // Number of columns for the mesh.
-		  columns: undefined,
+					columns_auto: true,
 
-		  columns_auto: true,
+					// Number of rows for the mesh.
+					rows: undefined,
 
-		  // Number of rows for the mesh.
-		  rows: undefined,
+					rows_auto: true,
+					zoom: 1,
+					xRange: 0.8,
+					yRange: 0.1,
+					zRange: 1.0,
+					ambient: 'rgba(85, 85, 85, 1)',
+					diffuse: 'rgba(255, 255, 255, 1)',
+					background: 'rgb(255, 255, 255)',
+					speed: 0.0002,
+					fluctuationSpeed: 0.5,
+					fluctuationIntensity: 0,
+					onRender: function () {},
+					floorPosition: false,
+					draw: true
+				},
 
-		  rows_auto: true,
-		  zoom: 1,
-		  xRange: 0.8,
-		  yRange: 0.1,
-		  zRange: 1.0,
-		  ambient: 'rgba(85, 85, 85, 1)',
-		  diffuse: 'rgba(255, 255, 255, 1)',
-		  background: 'rgb(255, 255, 255)',
-		  speed: 0.0002,
-		  fluctuationSpeed: 0.5,
-		  fluctuationIntensity: 0,
-		  onRender: function () {
-		  },
-		  floorPosition: false,
-		  draw: true
+				lights: {
+					// How many light sources belong to this light.
+					count: 1,
 
-		},
+					xyScalar: 1,
 
+					// Position of light source.
+					zOffset: 100,
 
-		lights: {
+					ambient: 'rgba(255,0,102, 1)',
+					diffuse: 'rgba(255,136,0, 1)',
+					speed: 0.010,
+					gravity: 1200,
 
-		  // How many light sources belong to this light.
-		  count: 1,
+					// Dampening of light's movements.
+					dampening: 0.95,
 
-		  xyScalar: 1,
+					minLimit: 10,
+					maxLimit: null,
+					minDistance: 20,
+					maxDistance: 400,
+					autopilot: false,
+					draw: false, //show circle
+					bounds: FSS.Vector3.create(),
+					step: FSS.Vector3.create(
+						Math.randomInRange(0.2, 1.0),
+						Math.randomInRange(0.2, 1.0),
+						Math.randomInRange(0.2, 1.0)
+					)
+				},
 
-		  // Position of light source.
-		  zOffset: 100,
+				// specify the thickness, color, stroke, etc.
+				line: {
+					fill: "rgba(0, 0, 0, 0)",
+					thickness: 1,
+					fluctuationIntensity: 0,
+					fluctuationSpeed: 0.5,
+					draw: false
+				},
 
-		  ambient: 'rgba(255,0,102, 1)',
-		  diffuse: 'rgba(255,136,0, 1)',
-		  speed: 0.010,
-		  gravity: 1200,
+				// Set the point attributes for the vertex.
+				vertex: {
+					// Radius of vertice circle.
+					radius: 0,
 
-		  // Dampening of light's movements.
-		  dampening: 0.95,
+					fill: "rgba(0, 0, 0, 0)",
 
-		  minLimit: 10,
-		  maxLimit: null,
-		  minDistance: 20,
-		  maxDistance: 400,
-		  autopilot: false,
-		  draw: false, //show circle
-		  bounds: FSS.Vector3.create(),
-		  step: FSS.Vector3.create(
-		    Math.randomInRange(0.2, 1.0),
-		    Math.randomInRange(0.2, 1.0),
-		    Math.randomInRange(0.2, 1.0)
-		  )
+					// Fluctuates opacity of vertex.
+					fluctuationSpeed: 0.5,
 
-		},
+					fluctuationIntensity: 0,
+					strokeWidth: 0,
+					strokeColor: "rgba(0, 0, 0, 0)",
 
-		// specify the thickness, color, stroke, etc.
-		line: {
-
-		  fill: "rgba(0, 0, 0, 0)",
-		  thickness: 1,
-		  fluctuationIntensity: 0,
-		  fluctuationSpeed: 0.5,
-		  draw: false
-
-		},
-
-		// Set the point attributes for the vertex.
-		vertex: {
-
-		  // Radius of vertice circle.
-		  radius: 0,
-
-		  fill: "rgba(0, 0, 0, 0)",
-
-		  // Fluctuates opacity of vertex.
-		  fluctuationSpeed: 0.5,
-
-		  fluctuationIntensity: 0,
-		  strokeWidth: 0,
-		  strokeColor: "rgba(0, 0, 0, 0)",
-
-		  // Instead of setting alpha channel to zero
-		  // Set draw to false to avoid computing.
-		  draw: false
-
-		}
-
+					// Instead of setting alpha channel to zero
+					// Set draw to false to avoid computing.
+					draw: false
+				}
+			});
 		});
-		});
-
-	</script>
-<!--Background end-->
+		</script>
 
 	</body>
 </html>
